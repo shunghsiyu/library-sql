c899f0b763e5277079b88e5249851c56c37aa25d
shunghsiyu <shunghsiyu@gmail.com>
2015-05-06 17:34:04 -0400

    Add routing for admin

---
+++ js/admin.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var libraryAdminApp = angular.module('libraryAdminApp', ['angularMoment', 'ui.router', 'ui.bootstrap', 'ui.select']);
+
+libraryAdminApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider.state('home', {
+        url: '/home',
+        templateUrl: '/partial/library.html'
+    }).state('branches', {
+        url: '/branches',
+        templateUrl: '/partial/branches.html'
+    }).state('addReader', {
+        url: '/addReader',
+        templateUrl: '/partial/add_reader.html'
+    }).state('addCopy', {
+        url: '/addCopy',
+        templateUrl: '/partial/add_copy.html'
+    }).state('searchCopies', {
+        url: '/searchCopies',
+        templateUrl: '/partial/search_copies.html'
+    });
+});
