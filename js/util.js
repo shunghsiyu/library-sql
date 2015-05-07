bd7a2504fdccbf260f9c3b32f82488da04397b62
shunghsiyu <shunghsiyu@gmail.com>
2015-05-07 11:50:07 -0400

    Add custom filter in libraryUtilApp

--- js/util.js
+++ js/util.js
@@ -15,4 +15,22 @@ libraryUtilApp.filter('reservationStatus', function () {
         var max_borrow_days = 20;
         return moment(bDatetime).add(max_borrow_days, 'days')
     }
+}).filter('adminCopyAvailable', function () {
+    return function(copy) {
+        if(copy.borrower) {
+            return false;
+        }
+        return copy.reserver === null;
+    }
+}).filter('adminCopyStatus', function () {
+    return function(copy) {
+        if(copy.borrower) {
+            return '\u2718 Borrowed by ' + copy.borrower.name + ' (id: ' + copy.borrower.reader_id + ')';
+        }
+        else if(copy.reserver) {
+            return '\u2718 Reserved by ' + copy.reserver.name + ' (id: ' + copy.reserver.reader_id + ')';
+        } else {
+            return '\u2713 Available'
+        }
+    }
 });
