6ed7ed82d7694c574a0e6f92b2e447789f74f9ba
shunghsiyu <shunghsiyu@gmail.com>
2015-05-07 01:24:02 -0400

    Add Controller addCopyCtrl for addCopy #6

--- js/admin.js
+++ js/admin.js
@@ -21,6 +21,10 @@ libraryAdminApp.factory('averageFineResource', function($resource) {
                 'get': {method: 'GET'}
             }, {stripTrailingSlashes: true});
     };
+}).factory('booksResource', function($resource) {
+    return $resource('/api/books/');
+}).factory('copiesResource', function($resource) {
+    return $resource('/api/copies/');
 });
 
 libraryAdminApp.controller('libraryInfoCtrl', function($scope, averageFines) {
@@ -36,6 +40,19 @@ libraryAdminApp.controller('libraryInfoCtrl', function($scope, averageFines) {
     $scope.branches = branchesData.branches;
     $scope.load = function(branch) {
         $scope.branch = branchResource(branch.lib_id).get();
+    }
+}).controller('addCopyCtrl', function($scope, booksData, branchesData, copiesResource) {
+    $scope.books = booksData.books;
+    $scope.branches = branchesData.branches;
+    $scope.add = function() {
+        if($scope.addCopyForm.$invalid) {
+            return;
+        }
+        var selected = $scope.selected;
+        copiesResource.save({
+            book_id: selected.book.book_id,
+            lib_id: selected.branch.lib_id
+        });
     }
 });
 
@@ -69,7 +86,16 @@ libraryAdminApp.config(['$resourceProvider', function($resourceProvider) {
         controller: 'addReaderCtrl'
     }).state('addCopy', {
         url: '/addCopy',
-        templateUrl: '/partial/add_copy.html'
+        templateUrl: '/partial/add_copy.html',
+        controller: 'addCopyCtrl',
+        resolve: {
+            branchesData: function(branchesResource) {
+                return branchesResource.get().$promise;
+            },
+            booksData: function(booksResource) {
+                return booksResource.get().$promise;
+            }
+        }
     }).state('searchCopies', {
         url: '/searchCopies',
         templateUrl: '/partial/search_copies.html'
