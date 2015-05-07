af8766fd128938eaa20d8d706aaac8f9d8a294fd
shunghsiyu <shunghsiyu@gmail.com>
2015-05-07 13:23:15 -0400

    Update library.py
    Output 0 for fine in the case where fine is null

--- library.py
+++ library.py
@@ -293,9 +293,9 @@ class Readers(Table):
         except sqlite3.Error:
             raise
 
-        result = [dict(reader=Readers.get(conn, row[0]), fine=row[1])
+        result = [dict(reader=Readers.get(conn, row[0]), fine=Decimal('0') if row[1] is None else row[1])
                   for row in rows]
-
+        
         return result
 
 
